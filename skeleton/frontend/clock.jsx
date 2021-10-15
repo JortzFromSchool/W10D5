4ce09877f143bade33473c9ec239f49f7d738756
JortzFromSchool <johnnydortiz@gmail.com>
2021-10-15 15:20:07 -0400

    clock functional, ready for styling

--- skeleton/frontend/clock.jsx
+++ skeleton/frontend/clock.jsx
@@ -3,12 +3,33 @@ import React from "react";
 class Clock extends React.Component {
     constructor(props) {
         super(props)
+        let time = new Date();
+        this.handle;
+        this.state = { 
+            clock: time
+        }
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount(){
+        this.handle = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.handle);
     }
 
     render() {
         return (
-            <h1>Clock Widget</h1>
+            <div>
+                <h1>Clock Widget</h1>
+                <div>{this.state.clock.getHours()}:{this.state.clock.getMinutes()}:{this.state.clock.getSeconds()}</div>
+            </div>
         )
+    }
+
+    tick() {
+        this.setState({clock: new Date()});
     }
 }
 
