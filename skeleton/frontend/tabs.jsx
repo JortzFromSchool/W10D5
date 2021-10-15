1a139fc5f811dfd9c4150f69c39cf48f393e937f
JortzFromSchool <johnnydortiz@gmail.com>
2021-10-15 16:44:10 -0400

    looking into how to pass our onClick function a variable

--- skeleton/frontend/tabs.jsx
+++ skeleton/frontend/tabs.jsx
@@ -3,7 +3,34 @@ import React from "react";
 class Tabs extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            selected: 0
+        };
+        this.changeSelected = this.changeSelected.bind(this);
     };
+    
+    changeSelected(idx) {
+        this.setState({selected: idx});
+    }
+
+    render(){
+        return (
+            <div>
+                <ul>
+                    {this.props.tabs.map((el, idx) => {
+                        <header onClick={this.changeSelected(idx)}>
+                            <h1>{el.title}</h1>
+                        </header>
+                    })}
+                </ul>
+                <article>
+                    <p>{this.props.tabs[this.selected].content}</p>
+                </article>
+            </div>
+        );
+    }
+
+    
 }
 
 export default Tabs;
--- skeleton/widgets.jsx
+++ skeleton/widgets.jsx
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
 import Tabs from './frontend/tabs';
 
+const tab1 = {title: "first", content:"this is the first tab"};
+const tab2 = {title: "second", content:"this is the second tab"};
+const tab3 = {title: "third", content:"this is the third tab"};
+
+const tabs = [tab1, tab2, tab3];
+
 const Root = (props) => {
   return (
   <div>
       <Clock />
-      <Tabs />
+      <Tabs tabs={props.tabs} />
   </div>) };
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   // root.append(Root);
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root tabs={tabs} />, root);
 });
 
