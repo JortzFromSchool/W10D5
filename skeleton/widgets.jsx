aca9ab172ac987906f5c0c217b1cffde4d6f3b1d
JortzFromSchool <johnnydortiz@gmail.com>
2021-10-15 16:19:05 -0400

    Tabs currently errors out clock

--- skeleton/widgets.jsx
+++ skeleton/widgets.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
+import Tabs from './frontend/tabs';
 
-const Root = (props) => { return <div ><Clock /></div> };
+const Root = (props) => {
+  return (
+  <div>
+      <Clock />
+      <Tabs />
+  </div>) };
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
