76e10c0e4c4702f44d8e0c82a6ac13520a76fb44
neil <neil.pandya.np@gmail.com>
2021-10-15 14:51:26 -0400

    Setup complete.

--- skeleton/widgets.jsx
+++ skeleton/widgets.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Congrats from './congrats';
 
+const Root = (props) => { return <div >Working</div> };
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Congrats/>, root);
+  // root.append(Root);
+  ReactDOM.render(<Root />, root);
 });
+
